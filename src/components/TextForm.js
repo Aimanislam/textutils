5cbabfbc0bf24a287dd0b476d5519eb94b5543c9
Aimanislam <57367836+Aimanislam@users.noreply.github.com>
2022-04-17 23:13:19 +0500

    added project

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,26 +23,28 @@ export default function TextForm(props) {
     console.log("On change");
     setText(event.target.value)
   }
+
   const [text, setText] = useState('Enter text here');
   return (
-    <>
-    <div className="container">
-   <h1>{props.heading}</h1>
-  <div className="mb-3">
-  <textarea className="form-control" value={text} onchange={handleOnChange} id="myBox" rows="8"></textarea>
-</div>
-<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className="btn btn-primary mx-1" onClick={handleloClick}>Convert to Lowercase</button>
-<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-    </div>
-    <div className="container">
-      <h1>Your text summary</h1>
-      <p>{text.split(" ").length} word and {text.length}characters</p>
-      <p>{0.008 * text.split(" ").length} Minutes read</p>
-    <h2>Preview</h2>
-    <p>{text}</p>
-    </div>
-    </>
-  )
-}
+            <>
+                  <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                <textarea className="form-control" value={text} onchange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white'}}} id="myBox" rows="8"></textarea>
+              </div>
+              <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+              <button className="btn btn-primary mx-1" onClick={handleloClick}>Convert to Lowercase</button>
+              <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+               </div>
+                <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} word and {text.length}characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                 <p>{text}</p>
+                </div>
+                  </>
+                )
+      }
+         
 
